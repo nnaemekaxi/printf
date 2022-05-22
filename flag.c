11ce8fed698c2c764d949a6e64fba447caa5c6d6
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-05-22 16:22:47 -0700

    main header

--- flag.c
+++ flag.c
@@ -29,3 +29,54 @@ int get_flag(char s, flags_t *f)
 	}
 	return (x);
 }
+/**
+ * get_modifier - finds the modifier func
+ * @s: the format string
+ * @f: the parameters struct
+ *
+ * Return: if modifier was valid
+ */
+
+int get_modifier(char *s, flags_t *f)
+{
+	int x = 0;
+
+	switch (*s)
+	{
+	case 'h':
+		x = f->h_modifier = 1;
+		break;
+	case 'l':
+		x = f->l_modifier = 1;
+		break;
+	}
+	return (x);
+}
+
+/**
+ * get_width - gets the width from the format string
+ * @s: the format string
+ * @f: the parameters struct
+ * @list: the argument pointer
+ *
+ * Return: new pointer
+ */
+
+char *get_width(char *s, flags_t *f, va_list list)
+/* should this function use char **s and modify the pointer? */
+{
+	int y = 0;
+
+	if (*s == '*')
+	{
+		y = va_arg(list, int);
+		s++;
+	}
+	else
+	{
+		while (_isdigit(*s))
+			y = y * 10 + (*s++ - '0');
+	}
+	f->width = y;
+	return (s);
+}
